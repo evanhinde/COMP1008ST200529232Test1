2ca642ebf780879ece7005051666dd11ff69b8f7
evanhinde <200529232@student.georgianc.on.ca>
2023-02-02 16:04:44 -0500

    question 3 complete

--- src/Clothing.java
+++ src/Clothing.java
@@ -7,4 +7,44 @@ public class Clothing {
     private String description;
     private Double price;
     private String colour;
+
+    public Clothing (String brand, String description, Double price, String colour)
+    {
+        this.brand = brand;
+        this.description = description;
+        this.price = price;
+        this.colour = colour;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public void setPrice(Double price) {
+    this.price = price;}
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
 }
+
+
