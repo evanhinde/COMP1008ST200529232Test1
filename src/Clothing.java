1c5ed0dcb88e75e6139f422619d3b4f167ee8646
evanhinde <200529232@student.georgianc.on.ca>
2023-02-02 15:58:08 -0500

    question 2 complete

---
+++ src/Clothing.java
@@ -0,0 +1,10 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class Clothing {
+
+    private String brand;
+    private String description;
+    private Double price;
+    private String colour;
+}
