cc57d88c1b48ab5b063826f45fe9fe3e43c86812
evanhinde <200529232@student.georgianc.on.ca>
2023-02-02 17:05:39 -0500

    question 6 partially complete

--- src/Closet.java
+++ src/Closet.java
@@ -1,6 +1,18 @@
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.Arrays;
 public class Closet {
-  //  private ArrayList<Clothing>;
+    private ArrayList<Clothing> clothes;
+
+    public Closet()
+    {
+        clothes = new ArrayList<>();
+        List<String> brands = Clothing.getValidBrands();
+        List<String> descriptions = Clothing.getValidDescriptions();
+        List<String> colours = Clothing.getValidColours();
+    }
+
+
+
+
 }
